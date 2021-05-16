7135190d234a02c61e571ee425d4657b8be984f7
Eduardo Meli <80851293+edu-m@users.noreply.github.com>
2021-05-16 22:03:18 +0200

    added comments

--- crop.py
+++ crop.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
-# abox = (361, 210, 1863, 1338)
+abox = (361, 210, 1863, 1338)
 bbox = (165, 96, 856, 615)
 cbox = (136, 1, 1000, 554)
 count = 1
-while count < 72:
+while count < 200: # the limit is arbitrary, maybe auto-detect?
     c = "% s" % count
-    im = Image.open(str(c) + ".png")
+    im = Image.open(str(c) + ".png") # todo: auto-detect file format on target folder
     region = im.crop(cbox)
     region.save(str(c) + "A.png")
     count = count + 1
